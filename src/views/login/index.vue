b6dedc7551d1e99b96c1bb2af896247c7d9744c2
Cristian <cuentagit1111@gmail.com>
2025-02-14 15:15:08 -0500

    Login view added

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,9 +3,53 @@
 </script>
 
 <template>
-  <main>
-    <h1>Login</h1>
+  <main :class="$style['container']">
+    <h1>Dar click para iniciar sesión con Google</h1>
+    <aside :class="$style['google-container']">
+      <img src="@/assets/images/google-icon.webp" />
+      <p>Ingresar con Google</p>
+    </aside>
   </main>
 </template>
 
-<style module></style>
+<style module>
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin: 0px 15px;
+  height: 70vh;
+  gap: 27px;
+}
+
+.container h1 {
+  margin: 0px;
+  text-decoration: underline;
+  text-align: center;
+  font-size: 21px;
+}
+
+.google-container {
+  display: flex;
+  align-items: center;
+  padding: 5px 20px;
+  /* padding: 10px; */
+  border: 1px solid #1a1a32;
+  border-radius: 6px;
+  cursor: pointer;
+  gap: 5px;
+}
+
+.google-container img {
+  width: 50px;
+  height: 50px;
+}
+
+.google-container p {
+  color: #1a1a32;
+  font-size: 14px;
+  font-weight: 600;
+  margin: 0px;
+}
+</style>
